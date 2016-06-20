77d20aef4b1082dd5fe5ff3ee2f18f796f79fb59
KamilZimaWAT <kamil.zima@student.wat.edu.pl>
2016-06-20 21:26:14 +0200

    Dodane klas Modelu dla aplikacji

---
+++ KursMVC/Models/Kurs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KursMVC.Models
+{
+    public class Kurs
+    {
+        public int KursId { get; set; }
+        public int KategoriaId { get; set; }
+        public string TytulKursu { get; set;}
+        public string AutorKursu { get; set; }
+        public DateTime DataDodania { get; set; }
+        public string NazwaPlikuObrazka { get; set; }
+        public int OpisKursu { get; set; }
+        public decimal CenaKursu { get; set; }
+        public bool Bestseller { get; set; }
+        public bool Ukryty { get; set; }    
+
+        public virtual Kategoria Kategoria { get; set; }    
+
+    }
+}
---
+++ KursMVC/Models/PozycjaZamowienia.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.AccessControl;
+
+namespace KursMVC.Models
+{
+    internal class PozycjaZamowienia
+    {
+        public int PozycjaZamowieniaId { get; set; }
+        public int ZamowienieId { get; set; }
+        public int KursId { get; set; }
+        public int Ilosc { get; set; }
+        public decimal CenaZakupu { get; set; }
+
+        public virtual Kurs Kurs { get; set; }
+        public virtual Zamowienie Zamowienie { get; set; }
+    }
+}
